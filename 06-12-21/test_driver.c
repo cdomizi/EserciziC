5c69360a2f4e8b430ec2893848f58f41defee11c
cdomizi <domizicesare@gmail.com>
2021-12-06 15:29:33 +0100

    Aggiorna test_driver.c

--- 06-12-21/test_driver.c
+++ 06-12-21/test_driver.c
@@ -1,1 +1,20 @@
+#include "quiz.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+	int a, b;
+	if (argc != 3) {
+		printf("Usage: test_driver <int_a> <int_b>\n");
+		exit(-1);
+	}
+	a = atoi(argv[1]);
+	if (a == 0) {
+		printf("First argument must be an integer\n");
+	}
+	b = atoi(argv[2]);
+	if (b == 0) {
+		printf("Second argument must be an integer\n");
+	}
+	printf("quiz(%i, %i) = %i\n", a, b, quiz(a,b));
+}
